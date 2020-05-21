3caf524e19e627ff4317504da40ff8379649a599
David C. Li <sunnydavidli@gmail.com>
2020-05-21 11:01:50 +0800

    my initial commit with some basic codes.

---
+++ pythonWelcome.py
@@ -0,0 +1,12 @@
+class Welcome:
+    def __init__(self):
+        pass
+        
+    def display(self):
+        output = "welcome to Git for System Engineering!"
+        print(output)
+        
+ 
+welcome = Welcome()
+welcome.display()
+
